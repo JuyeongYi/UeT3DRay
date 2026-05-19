36a03ce0938502dd8f2bf0c363847b23b792bbe7
Jooyoung Lee <jylee@krafton.com>
2026-05-19 19:13:03 +0900

    feat(app): pin 'changed-from-default' heuristic

---
+++ tests/core/app/test_pin_status.py
@@ -0,0 +1,44 @@
+from t3dgraph.core.base.graph_model import Pin
+from t3dgraph.core.app.pin_status import is_changed_from_default
+
+
+def _pin(cpp, dv):
+    return Pin(name="P", cpp_type=cpp, direction="Input", default_value=dv)
+
+
+def test_none_default_not_changed():
+    assert is_changed_from_default(_pin("double", None)) is False
+
+
+def test_bool_false_not_changed():
+    assert is_changed_from_default(_pin("bool", "False")) is False
+    assert is_changed_from_default(_pin("bool", "false")) is False
+
+
+def test_bool_true_changed():
+    assert is_changed_from_default(_pin("bool", "True")) is True
+
+
+def test_numeric_zero_not_changed():
+    assert is_changed_from_default(_pin("double", "0.000000")) is False
+    assert is_changed_from_default(_pin("int32", "0")) is False
+
+
+def test_numeric_nonzero_changed():
+    assert is_changed_from_default(_pin("double", "1.000000")) is True
+
+
+def test_fname_none_not_changed():
+    assert is_changed_from_default(_pin("FName", "None")) is False
+
+
+def test_fname_value_changed():
+    assert is_changed_from_default(_pin("FName", "IKTarget")) is True
+
+
+def test_empty_struct_not_changed():
+    assert is_changed_from_default(_pin("FVector", "()")) is False
+
+
+def test_struct_with_value_changed():
+    assert is_changed_from_default(_pin("FQuat", "(X=0.0,W=1.0)")) is True
